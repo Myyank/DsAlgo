569bcf81adf891da1aec600b198a29d0f57f8b2c
Myyank <mayyank.libra@gmail.com>
2021-03-09 19:59:56 +0530

    Create MergeSort.cpp

---
+++ Recursion/MergeSort.cpp
@@ -0,0 +1,87 @@
+/*
+Sort an array A using Merge Sort.
+Change in the input array itself. So no need to return or print anything.
+Input format :
+Line 1 : Integer n i.e. Array size
+Line 2 : Array elements (separated by space)
+Output format :
+Array elements in increasing order (separated by space)
+Constraints :
+1 <= n <= 10^3
+Sample Input 1 :
+6 
+2 6 8 5 4 3
+Sample Output 1 :
+2 3 4 5 6 8
+Sample Input 2 :
+5
+2 1 5 2 3
+Sample Output 2 :
+1 2 2 3 5 
+*/
+
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort1(int arr[], int l, int r)
+{
+    if (l >= r)
+    {
+        return;
+    }
+    int m = (l + r - 1) / 2;
+    mergeSort1(arr, l, m);
+    mergeSort1(arr, m + 1, r);
+    merge(arr, l, m, r);
+}
+
+void mergeSort(int input[], int size)
+{
+
+    int start = 0, end = size - 1;
+
+    mergeSort1(input, start, end);
+}
